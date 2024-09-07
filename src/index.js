a2620bf09cc0ed3702cfeaf458b7035cf577d5af
amirani18 <112004077+amirani18@users.noreply.github.com>
2024-09-07 15:15:23 -0400

    begin index.ts as the "main" file taking in url input through the cli

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Import necessary modules
-import { URL } from 'url';
-
+var url_1 = require("url");
 // Function to validate if the input is a valid URL
-function isValidUrl(input: string): boolean {
+function isValidUrl(input) {
     try {
-        new URL(input);  // If URL constructor doesn't throw, it's valid
+        new url_1.URL(input); // If URL constructor doesn't throw, it's valid
         return true;
-    } catch (err) {
+    }
+    catch (err) {
         return false;
     }
 }
-
 // Main function to handle URL input from command line
 function main() {
     // Get the URL from the command-line arguments (process.argv[2] is the first argument after 'node' and the script name)
-    const urlInput = process.argv[2];
-
+    var urlInput = process.argv[2];
     if (!urlInput) {
         console.error('Error: Please provide a URL as an argument.');
-        process.exit(1);  // Exit the process with an error code
+        process.exit(1); // Exit the process with an error code
     }
-
     // Validate the URL
     if (isValidUrl(urlInput)) {
-        console.log(`The URL you provided is valid: ${urlInput}`);
-    } else {
+        console.log("The URL you provided is valid: ".concat(urlInput));
+    }
+    else {
         console.error('Error: Invalid URL format.');
-        process.exit(1);  // Exit the process with an error code
+        process.exit(1); // Exit the process with an error code
     }
 }
-
 // Run the main function
 main();
